a44cf7728ba8c5c2b70bc0ef863dabe3ef7ffeff
iesb <joaopedronunes358@gmail.com>
2020-10-07 19:04:17 -0300

    adicionando ao git

---
+++ Atividades/Atividade03/02-Ponto2Ponto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace par
+{
+    class Program
+    {
+        const int porta = 7000;
+
+        const string ip = "10.0.2.255";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+                UdpClient par = new UdpClient(porta);
+                Console.WriteLine("Para sair, pressione CTRL+C.");
+
+                while (true)
+                {
+                    byte[] bytesRecebidos = par.Receive(ref endPoint);
+                    Console.WriteLine("Mensagem recebida: {0}", Encoding.ASCII.GetString(bytesRecebidos));
+                }
+            }
+            catch (Exception)
+            {
+                UdpClient par = new UdpClient();
+                par.EnableBroadcast = true;
+
+                Console.WriteLine("Envie uma mensagem. Para sair, pressione ENTER.");
+                Console.Write("> ");
+                string msg = Console.ReadLine();
+
+                while (msg.Length > 0)
+                {
+                    byte[] bytes = Encoding.ASCII.GetBytes(msg);
+                    par.Send(bytes, bytes.Length, ip, porta);
+
+                    Console.Write("> ");
+                    msg = Console.ReadLine();
+                }
+
+                par.Close();
+            }
+        }
+    }
+}
---
+++ Aulas/01-ClienteServidor/Servidor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace servidor
+{
+    class Program
+    {
+
+        const int porta = 7000;
+
+        static void Main(string[] args)
+        {
+            TcpListener servidor = new TcpListener(IPAddress.Any, porta);
+
+            Console.WriteLine("Servidor no ar!");
+            servidor.Start();
+
+            while (true)
+            {
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Console.WriteLine("Conexao estabelecida!");
+
+                byte[] bytes = new byte[1024];
+                NetworkStream stream = cliente.GetStream();
+                
+                while (true)
+                {
+                    int bytesRead = stream.Read(bytes, 0, bytes.Length);
+
+                    if (bytesRead == 0) {
+                        break;
+                    }
+                    
+                    Console.WriteLine("Mensagem recebida: {0}", Encoding.ASCII.GetString(bytes, 0, bytesRead));
+                }
+                
+                stream.Close();
+                cliente.Close();
+                Console.WriteLine("Conexao encerrada!");
+            }
+
+            // servidor.Stop();
+        }
+    }
+}
